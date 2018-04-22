e20ebb930a4cbcd55c97d38b76937bda702c000b
Attila Večerek <attila.vecerek@gmail.com>
2018-04-22 08:20:41 +0200

    Improves styling

--- app/javascript/packs/GoodLuck/index.js
+++ app/javascript/packs/GoodLuck/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import goodLuck from './liam_neeson_good_luck';
 
 export default function GoodLuck() {
-  return <img src="https://i.gifer.com/2uq.gif" />;
+  return <div style={{ height: "340px" }}>
+    <img src="https://i.gifer.com/2uq.gif" />
+  </div>;
 }
--- app/javascript/packs/LiamNeeson/index.js
+++ app/javascript/packs/LiamNeeson/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import findAndKill from './liam_neeson_find_and_kill.gif';
 
 export default function LiamNeeson() {
-  return <img src="https://media1.tenor.com/images/a38b7ac02a752bd50350ca02a25faa1d/tenor.gif" />;
+  return <div style={{ height: "340px" }}>
+    <img src="https://media1.tenor.com/images/a38b7ac02a752bd50350ca02a25faa1d/tenor.gif" />
+  </div>;
 }
